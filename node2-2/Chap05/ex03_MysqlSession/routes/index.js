35d6dc0ee06c11a8a4d3c729805187a1e2110e0b
DESKTOP-BB105KM\user <goodluck3586@gsm.hs.kr>
2020-11-20 14:50:34 +0900

    2020-11-20 수업

---
+++ node2-2/Chap05/ex03_MysqlSession/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function timeLog(req, res, next) {
+  console.log('req.session: ', req.session);
+  next();
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+ 
+router.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
+
+module.exports = router;
